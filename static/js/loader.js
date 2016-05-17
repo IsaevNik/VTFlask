d569548d9eedd183a230f8f6ad37c7d0590d6103
Nikita <isv.nikita@gmail.com>
2016-05-17 19:47:02 +0400

    add slider

--- static/js/loader.js
+++ static/js/loader.js
@@ -2,9 +2,20 @@ $(document).ready(function(){
 	$("main").load("main.html");
     //$(".main_button").attr("id", "current_page");
     $("main").attr("id", "main");
-	$('.carousel').carousel({
-		interval: 6000
-	})
+	$(".owl-carousel").owlCarousel({
+		navigation : false, // показывать кнопки next и prev 
+		autoPlay: 3000,
+		//slideSpeed : 1000,
+		paginationSpeed : 1500,
+	 
+		items : 1, 
+		itemsDesktop : false,
+		itemsDesktopSmall : false,
+		itemsTablet: false,
+		itemsMobile : false
+	});
+	var owl = $(".owl-carousel").data('owlCarousel');
+	owl.destroyControlls();
 });
 
 $(".navbar-nav .li").click(function(){
