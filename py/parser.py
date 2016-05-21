48f9b4f77574d5af5814971d8babb11336f6778e
Isaev Nikita <isv.nikita@gmail.com>
2016-05-21 17:05:49 +0400

    Рабочий скрипт, который скачивает изображения

--- py/parser.py
+++ py/parser.py
@@ -1,6 +1,7 @@
 import urllib.request
 import re
 import os
+import shutil
 
 def print_url_error(url):
     print('Ошибка при открытии ', url)
@@ -19,10 +20,10 @@ def main():
     else:
         page = url_d.read().decode('utf-8')
         print_url_sucsees(main_url)
-        
+    lev = 11    
     result = re.findall(r'\s*<a title="(.*?)" style="display\:block;" class="mainlevel" href="/(.*?)"', page) #Ищем главные разделы каталога товаров
     #site_dir = [{'name':result[i][0],'href':result[i][1],'sublevels':[]} for i in result] #добавляем список разделов в виде словаря {имя,ссылка,список товаров}
-    site_dir = [{'name':result[1][0],'href':result[1][1],'sublevels':[]}] #Для отладки берём только первый раздел каталога
+    site_dir = [{'name':result[lev][0],'href':result[lev][1],'sublevels':[]}] #Для отладки берём только первый раздел каталога
     
     
     for level in site_dir:
@@ -37,10 +38,20 @@ def main():
             print_url_sucsees(url_level)
             
         result = re.findall(r'\s*<a title="(.*?)" style="display\:block;" class="sublevel" href="/(.*?)"', page) #Ищем названия подраздела товаровов и ссылку на него в каждом разделе каталога
-        level['sublevels'] = [{'name':i[0],'href':i[1],'products':[]} for i in result] #добавляем в каждый раздел каталога список подразделов в виде списка словарей {имя,ссылка,список товаров} 
+        if len(result) == 0:
+            level['sublevels'] = [{'name': level['name'], 'href': level['href'], 'products':[]}]
+        else:
+            level['sublevels'] = [{'name':i[0],'href':i[1],'products':[]} for i in result] #добавляем в каждый раздел каталога список подразделов в виде списка словарей {имя,ссылка,список товаров}
+        if os.path.exists(level['href']):
+            shutil.rmtree(level['href']) 
+        os.mkdir(level['href'])
 
-        for sublevel in level['sublevels']:
+        for j,sublevel in enumerate(level['sublevels']):
             url_sublevel = main_url + sublevel['href']
+            #sublevel_dir = sublevel['href'].split('/')[0]
+            sublevel_dir = sublevel['href'].split('/')[0]+str(j)#Если есть некрасивые ссылки
+            dest_dir = os.path.join(level['href'],sublevel_dir)
+            os.mkdir(dest_dir)
             try:
                 url_d = urllib.request.urlopen(url_sublevel)
             except:
@@ -50,19 +61,28 @@ def main():
                 print_url_sucsees(url_sublevel)
 
             names = re.findall(r'<a style="font-size\:16px; font-weight\:bold;" href="(?:.*?)">(.*?)</a>', page)
-            prices = re.findall(r'<p>\s*<(?:.*?)>\s*(Позвоните, чтобы уточнить цену|\d+\.\d+)', page)            
-            imgs = re.findall(r'<img src="(.*?)" height="\d*" width="\d*" class="browseProductImage"', page)
+            prices = re.findall(r'<p>\s*<(?:.*?)>\s*(Позвоните, чтобы уточнить цену|\d*\s*\d+\.\d+)', page)           
+            imgs = re.findall(r'<img src="(http\://tksiko\.ru/components/com\_virtuemart/.+?)"', page)
+            #imgs = re.findall(r'<img src="(.*?)" (?:height="\d*" width="\d*" class="browseProductImage)?"', page)
             sublevel['products'] = [{'name':product[0], 'price':product[1], 'img': product[2]} for product in list(zip(names,prices,imgs))] #Добавляем в каждый подраздел список товаров, в котором товары хранятся в виде словарей {имя,цена,ссылка на изображение} 
-            input("-----  Подраздел {} считан успешно. Перейти к следующему?".format(sublevel["name"]))
+            print(len(sublevel['products']))
             
-        input("Раздел {} считан успешно. Перейти к следующему?".format(level["name"]))
+            for i,product in enumerate(sublevel['products']):
+                img_dir = sublevel_dir+str(i)+'.jpg'
+                urllib.request.urlretrieve(product['img'], os.path.join(dest_dir, img_dir))
+            
+            
+            #input("-----  Подраздел {} считан успешно. Перейти к следующему?".format(sublevel["name"]))
         
-    for level in site_dir:
+            
+        #input("Раздел {} считан успешно. Перейти к следующему?".format(level["name"]))
+        
+    '''for level in site_dir:
         print('{} | ссылка {}'.format(level['name'], level['href']))
         for sublevel in level['sublevels']:
             print('-----  {} | ссылка {}'.format(sublevel['name'], sublevel['href']))
             for product in sublevel['products']:
-                print('++++++++++++ {name:20} {price:20} {img}'.format(**product))
+                print('++++++++++++ {name:20} {price:20} {img}'.format(**product))'''
     
 
             
