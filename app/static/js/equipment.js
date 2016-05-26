72e3e609434acac6375dd5c42c0414d755e59304
Isaev Nikita <isv.nikita@gmail.com>
2016-05-26 04:19:31 +0400

    Порядок в ветке

--- app/static/js/equipment.js
+++ app/static/js/equipment.js
@@ -1,13 +1,16 @@
 var main = function(){
 	
 	$(".sublevel").load("/passivnieComponentiVols");
-	
+	var telephon_pattern = /^(\+?\d{1}\(?\d{3}\)?\d{3}-?\d{2}-?\d{2})$/;
+ 	var name_pattern = /^([а-яА-Я ]+)$/;
+ 	var mail_pattern = /^[\w-\.]+@[\w-]+\.[a-z]{2,3}$/;
+ 	var sum = 0.0;
 
 	$(".show_email:checkbox").on('change', function(){
-		$('.email').toggleClass('hidden');
+		$('.email').slideToggle('slow');
 	});
 	$(".show_comment:checkbox").on('change', function(){
-		$('.comment').toggleClass('hidden');
+		$('.comment').slideToggle('slow');
 	});
 	$(".levels li").on('click',function(){
 		var href = $('a',this).attr('href');
@@ -17,6 +20,56 @@ var main = function(){
 		return false;
 	});
 	
+	$('.user-form input').on('focus',function(){
+ 		$(this).next().removeClass("glyphicon-ok");
+ 		$(this).next().removeClass("glyphicon-remove");
+ 		$(this).parent().removeClass("has-success");
+ 		$(this).parent().removeClass("has-error");
+ 	
+ 	});
+ 	$('#client_name').blur(function(){
+ 		var name = $(this).val();
+ 		if (name.match(name_pattern) != null){
+ 			$(this).parent().addClass("has-success");
+ 			$(this).next().addClass("glyphicon-ok");
+ 		} else {
+ 			$(this).parent().addClass("has-error");
+ 			$(this).next().addClass("glyphicon-remove");
+ 		}
+ 	});
+ 	
+ 	$('#client_telephone').blur(function(){
+ 		var telephon = $(this).val();
+ 		if (telephon.match(telephon_pattern)!=null){
+ 			$(this).parent().addClass("has-success");
+ 			$(this).next().addClass("glyphicon-ok");
+ 		} else {
+ 			$(this).parent().addClass("has-error");
+ 			$(this).next().addClass("glyphicon-remove");
+ 		}
+ 	});
+ 	$('#client_email').blur(function(){
+ 		var mail = $(this).val();
+ 		if (mail.match(mail_pattern)!=null){
+ 			$(this).parent().addClass("has-success");
+ 			$(this).next().addClass("glyphicon-ok");
+ 		} else {
+ 			$(this).parent().addClass("has-error");
+ 			$(this).next().addClass("glyphicon-remove");
+ 		}
+ 	});
+ 	
+ 	$('.user-form').focusin(function(){
+ 		$(this).toggleClass("focus-form");
+ 	});
+ 	$('.user-form').focusout(function(){
+ 		$(this).toggleClass("focus-form");
+ 	});
+
+ 	$('.client-cart').on('click','.remove-item',function(){
+ 		alert("1");
+ 		$(this).parent('.list-group-item').remove();
+ 	});
 }
 
 $(document).ready(main);
--- app/static/js/sublevel.js
+++ app/static/js/sublevel.js
@@ -1,8 +1,5 @@
 var main = function(){
 
-	
-
-	
 	$('.panel-heading').on('click',function(){
 		var parent = $(this).parent('.panel');
 		$('.collapse',parent).collapse('toggle');
@@ -22,14 +19,41 @@ var main = function(){
 	$(".input-group-addon.down").on('click', function(){
 		var input_val = +($(this).prev().val());
 		
-		if (input_val > 0){
+		if (input_val > 1){
 			$(this).prev().val(input_val-1);
 		} else {
-			$(this).prev().val(0);
+			$(this).prev().val(1);
 		};
 		
 	});
-	
+	$( ".shoping-cart" ).on('mousedown',function() {
+ 		$(this).addClass( "click" );
+ 	});
+ 	$( ".shoping-cart" ).on('mouseup',function() {
+ 		$( this ).removeClass( "click" );
+ 	});
+ 	$(".shoping-cart").on('click', function(){
+ 		var input_form = $(this).parent().prev();
+ 		var description = $(this).parents('.counter').prev();
+ 		var item_name = $("p",description).text();
+ 		var count = $("input", input_form).val();
+ 		var line = item_name + " кол-во." + count;
+
+ 		var newItem = $('<li class="list-group-item"></li')
+        .append('<span class="glyphicon glyphicon-remove-circle remove-item"></span>')
+        .append($('<span/>',{
+        	"class" : "count",
+        	text : count + " шт."
+        }))
+        .append($('<span/>',{
+        	"class" : "name-item",
+        	text : item_name
+        }));
+ 		//alert(line);
+ 		if (count != 0){
+ 			$('.client-cart').prepend(newItem);
+ 		}
+ 	});
 }
 
 $(document).ready(main);
